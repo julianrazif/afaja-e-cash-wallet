8855a8320f88d815653a5d4009b41cb6ec520f01
Julian Razif Figaro <julian.razif@gmail.com>
2023-02-16 00:06:39 +0700

    update integrator module

--- afaja-e-cash-wallet-integrator/src/main/java/com/julianraziffigaro/afajaecashwallet/integrator/Application.java
+++ afaja-e-cash-wallet-integrator/src/main/java/com/julianraziffigaro/afajaecashwallet/integrator/Application.java
@@ -1,8 +1,11 @@
 package com.julianraziffigaro.afajaecashwallet.integrator;
 
+import com.julianraziffigaro.afajaecashwallet.core.kafka.processor.ReactorKafkaProcessor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
@@ -12,15 +15,24 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
     "com.julianraziffigaro.afajaecashwallet.integrator",
     "com.julianraziffigaro.afajaecashwallet.core",
     "com.julianraziffigaro.afajaecashwallet.va",
-    "com.julianraziffigaro.afajaecashwallet.reactive.va"
-  },
-  exclude = {
-    JmxAutoConfiguration.class
+    "com.julianraziffigaro.afajaecashwallet.reactive.va",
+    "com.julianraziffigaro.afajaecashwallet.kafka.va"
   }
 )
 public class Application implements WebFluxConfigurer {
 
+  private final ReactorKafkaProcessor kafkaReceiverTemplate;
+
+  public Application(@Qualifier("vaReactorKafkaProcessorImpl") ReactorKafkaProcessor kafkaReceiverTemplate) {
+    this.kafkaReceiverTemplate = kafkaReceiverTemplate;
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public ApplicationRunner runner() {
+    return args -> kafkaReceiverTemplate.processor();
   }
 }
